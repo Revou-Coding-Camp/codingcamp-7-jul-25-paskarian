426f6a65ab863d5dc094bdaaa23012f6bc7292a9
paskarian <paskaaae999@student.untan.ac.id>
2025-07-11 21:32:13 +0700

    masuk ke js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let tasks = [];
+
+// Function to add a task
+function addTask() {
+    const taskInput = document.getElementById('task-input');
+    const dueDateInput = document.getElementById('due-date-input');
+}
+
+// Function to delete all tasks
+function deleteAllTask() {
+
+}
+
+// Function to delete a specific task
+function deleteTask(id) {
+
+}
+
+// Funtion to filter tasks
+function filterTasks() {
+
+}
+
+// Event Listener for the form submission
+document.getElementById('add-task-button').addEventListener('click', addTask)
